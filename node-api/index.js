593066efbc4871cb1345a3cceee84746ab9da9e7
Kunho Lee <gunho1020@gmail.com>
2018-04-14 13:37:58 +0900

    노드로 만나는 Hello World/Hello World 노드버전

--- node-api/index.js
+++ node-api/index.js
@@ -12,15 +12,30 @@ const result = math.sum(1, 2);
 console.log(result); // 3
 */
 
-// node.js 의 file system 모듈 사용
+/* node.js 의 file system 모듈 사용
 const fs = require('fs');
-/* 동기 방식
+// 동기 방식
 const data = fs.readFileSync('./data.txt', 'utf-8');
 console.log(data);
-*/
+
 // 비동기 방식
 const data = fs.readFile('./data.txt', 'utf-8', function(err ,data) {
   console.log(data);
 });
 console.log('line after fs.readFile');
+*/
 
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World\n');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
