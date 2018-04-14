0b9eba70120a3b2f635b74449a652a5b0e039177
Kunho Lee <gunho1020@gmail.com>
2018-04-15 00:15:59 +0900

    익스프레스(ExpressJS)기초/Hello world 익스프레스버전

--- express-example/index.js
+++ express-example/index.js
@@ -1,20 +1,10 @@
 const express = require('express');
 const app = express();
 
-function commonmw(req, res, next) {
-  console.log('commonmw');
-  next(new Error('error ouccered'));
-}
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
 
-function errormw(err, req, res, next) {
-  console.log(err.message);
-  // 에러를 처리하거나
-  next();
-}
-
-app.use(commonmw);
-app.use(errormw);
-
-app.listen(3000, function() {
-  console.log('Server is running');
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
 });
