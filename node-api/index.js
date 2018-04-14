3714bf1f904c10b01362cfe2d2f6d1f63f90da35
Kunho Lee <gunho1020@gmail.com>
2018-04-14 13:25:45 +0900

    NodeJS 기초/비동기 세계 1 - readFileSync

--- node-api/index.js
+++ node-api/index.js
@@ -1,11 +1,21 @@
-// node.js 기본 내장 모듈 사용
+/* node.js 기본 내장 모듈 사용
 const http = require('http');
 
 http.createServer();
+*/
 
-// 사용자 정의 모듈 사용
+/* 사용자 정의 모듈 사용
 const math = require('./math.js');
 
 const result = math.sum(1, 2);
 
 console.log(result); // 3
+*/
+
+// node.js 의 file system 모듈 사용
+const fs = require('fs');
+
+const data = fs.readFileSync('./data.txt', 'utf-8');
+
+console.log(data);
+
