d84058c8a1ea90240a558eb5ca6d1abad2e6c667
Kunho Lee <gunho1020@gmail.com>
2018-04-15 00:02:43 +0900

    익스프레스(ExpressJS)기초/다른 개발자가 만든 미들웨어 사용하기

--- express-example/index.js
+++ express-example/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 
 function logger(req, res, next) {
   console.log('i am logger');
-  // next();
+  next();
 }
 
 function logger2(req, res, next) {
@@ -13,6 +14,7 @@ function logger2(req, res, next) {
 
 app.use(logger);
 app.use(logger2);
+app.use(morgan('dev'));
 
 app.listen(3000, function() {
   console.log('Server is running');
