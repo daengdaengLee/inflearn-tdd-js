a8e01fb232bc23f1683d50b9a6f4c7b2d575daaa
Kunho Lee <gunho1020@gmail.com>
2018-04-15 21:24:17 +0900

    TDD로 하는 API서버 개발/사용자 수정 API 성공시

--- express-example/index.spec.js
+++ express-example/index.spec.js
@@ -30,7 +30,7 @@ describe('GET /users 는', () => {
     });
   });
 });
-describe('GET /users/1 는', () => {
+describe('GET /users/:id 는', () => {
   describe('성공시', () => {
     it('id가 1인 유저 객체를 반환한다.', done => {
       request(app)
@@ -56,7 +56,7 @@ describe('GET /users/1 는', () => {
     });
   });
 });
-describe('DELETE /users/1', () => {
+describe('DELETE /users/:id 는', () => {
   describe('성공시', () => {
     it('204를 응답한다.', done => {
       request(app)
@@ -74,7 +74,7 @@ describe('DELETE /users/1', () => {
     });
   });
 });
-describe('POST /users', () => {
+describe('POST /users 는', () => {
   describe('성공시', () => {
     let name = 'daniel';
     let body;
@@ -114,6 +114,20 @@ describe('POST /users', () => {
         .send({ name: 'daniel' })
         .expect(409)
         .end(done);
+    });
+  });
+});
+describe('PUT /users/:id 는', () => {
+  describe('성공시', () => {
+    it('변경된 name을 응답한다.', done => {
+      const name = 'chally';
+      request(app)
+        .put('/users/3')
+        .send({ name })
+        .end((err, res) => {
+          res.body.should.have.property('name', name);
+          done();
+        });
     });
   });
 });
