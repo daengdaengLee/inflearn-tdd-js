7bb0003387b8b10beaca04cd651cc5ed4691456e
Kunho Lee <gunho1020@gmail.com>
2018-04-15 09:01:11 +0900

    REST API란/첫 번째 API 만들기:사용자 목록 조회 API

--- express-example/index.js
+++ express-example/index.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const app = express();
+const margan = require('morgan');
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+const users = [
+  { id: 1, name: 'alice' },
+  { id: 2, name: 'bek' },
+  { id: 3, name: 'chris' },
+];
+
+app.use(margan('dev'));
+
+app.get('/users', (req, res) => {
+  res.json(users);
 });
 
-app.listen(3000, function () {
+app.listen(3000, () => {
   console.log('Example app listening on port 3000!');
 });
