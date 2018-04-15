f611ace4029e68611fc53ebfc3a1ef2a4f890cd2
Kunho Lee <gunho1020@gmail.com>
2018-04-15 10:05:51 +0900

    TDD로 하는 API서버 개발/사용자 목록 조회 API 테스트 코드 만들기2

--- express-example/index.js
+++ express-example/index.js
@@ -11,7 +11,12 @@ const users = [
 app.use(margan('dev'));
 
 app.get('/users', (req, res) => {
-  res.json(users);
+  req.query.limit = req.query.limit || 10;
+  const limit = parseInt(req.query.limit, 10);
+  if(Number.isNaN(limit)) {
+    return res.status(400).end();
+  }
+  res.json(users.slice(0, limit));
 });
 
 app.listen(3000, () => {
