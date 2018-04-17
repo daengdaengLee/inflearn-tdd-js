518a4ba16d3d77b061715f7758b6ea01df21d111
Kunho Lee <gunho1020@gmail.com>
2018-04-17 23:40:45 +0900

    데이터베이스/데이터베이스와 index 컨트롤러 연동 1

--- express-example/api/user/user.spec.js
+++ express-example/api/user/user.spec.js
@@ -3,10 +3,12 @@
 const should = require('should');
 const request = require('supertest');
 const app = require('../../');
+const models = require('../../models');
 
 describe('GET /users 는', () => {
   describe('성공시', () => {
-    it('유저 객체를 담은 배열로 응답한다.', done => {
+    before(() => models.sequelize.sync({ force: true }));
+    it.only('유저 객체를 담은 배열로 응답한다.', done => {
       request(app)
         .get('/users')
         .end((err, res) => {
